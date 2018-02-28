75e1d73dbf32558fc589890875871616c50c1b16
jpriggs <jprigdon@gmail.com>
2018-02-28 19:08:23 +0900

    Initial commit with views, css, template, and js

---
+++ manage.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+from quoteroulette import views
+from quoteroulette.views import app
+
+if __name__ == "__main__":
+    app.run()
---
+++ quoteroulette/static/js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  getQuote();
+  $("#new-quote").on('click', function() {
+    $("#new-quote").prop('disabled', true);
+    getQuote();
+  });
+});
+function getQuote() {
+  var url = 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous';
+  var currentQuote = '';
+  var currentAuthor = '';
+  var shareTitle = 'Quote Roulette: Home to all your favorite quotes!';
+  $.ajax({
+    url: url,
+    type: 'POST',
+    data: {},
+    dataType: 'json',
+    contentType: 'application/x-www-form-urlencoded',
+    success: function(data) {
+        console.log(data);
+        $("#text").text('"' + data.quote + '"');
+        if (data.author === '') {
+            data.author = 'Unknown';
+        }
+        $("#author").text('Author: ' + data.author);
+        $("#new-quote").prop('disabled', false);
+        currentQuote = data.quote;
+        currentAuthor = data.author;
+        $("#tweet-quote").attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes,QuoteRoulette&text=' + encodeURIComponent('"' + currentQuote + '" ' + '- ' + currentAuthor));
+    },
+    error: function(err) {
+      alert(err);
+    },
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-Mashape-Key", "cRIhjDCUYKmshR5Qox6jKaXv8AyUp1F8P1Cjsnbq2823lGw58Q")
+    }
+  });
+}
